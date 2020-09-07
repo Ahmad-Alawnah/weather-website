6a36748d0edbfc5ac6cefe24ef12ad9ddf7e5157
Ahamd Alawnah <aalawneh19@gmail.com>
2020-09-07 11:00:24 +0300

    added humidity info

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,7 @@ const forecast = (longitude, latitude, callback) =>{
             callback('Unable to find location', undefined)
         }
         else{
-            callback(undefined, body.current.weather_descriptions[0] + ', ' + "it is currently " + body.current.temperature + " degrees, but it feels like " + body.current.feelslike + " degrees")
+            callback(undefined, body.current.weather_descriptions[0] + ', ' + "it is currently " + body.current.temperature + " degrees, but it feels like " + body.current.feelslike + " degrees, " + "The humidity is "+ body.current.humidity+"%")
         }
     })
 }
